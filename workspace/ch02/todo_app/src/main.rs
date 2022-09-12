80e44a990f2397263752eedb955ddf213a0ad345
Erik Karlsson <erikdkarlsson8@gmail.com>
2022-09-11 20:29:24 -0700

    Start chapter 3.

--- workspace/ch02/todo_app/src/main.rs
+++ workspace/ch02/todo_app/src/main.rs
@@ -1,15 +1,33 @@
-use crate::to_do::structs::traits::create::Create;
-use crate::to_do::{to_do_factory, ItemTypes};
-
+mod state;
 mod to_do;
+mod processes;
 
+use serde_json::value::Value;
+use serde_json::Map;
+use state::{read_file, write_to_file};
+use std::env;
+use crate::processes::process_input;
+use crate::to_do::to_do_factory;
+
+#[allow(unused, unused_mut)]
 fn main() {
-    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "washing");
-    match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
-        ItemTypes::Done(item) => println!(
-            "it's a done item with the title: {}",
-            item.super_struct.title
-        )
+    let args: Vec<String> = env::args().collect();
+    let command: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state: Map<String, Value> = read_file(String::from("./state.json").as_str());
+    println!("{:?}", state);
+    let status: String;
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None => {
+            status = "pending".to_string();
+        }
     }
+    let item = to_do_factory(&status, title)
+        .expect(&status);
+    process_input(item, command.to_string(), &state)
+    // state.insert(title.to_string(), json!(status));
+    // write_to_file("./state.json", &mut state);
 }
