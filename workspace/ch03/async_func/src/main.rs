a35afd302ae935ad22b5501eed6749d526fdc59e
Erik Karlsson <erikdkarlsson8@gmail.com>
2022-09-12 19:28:25 -0700

    Working on async functions.

---
+++ workspace/ch03/async_func/src/main.rs
@@ -0,0 +1,16 @@
+use std::{thread, time};
+use futures::executor::block_on;
+
+async fn do_something(number:i8) -> i8 {
+    println!("number {} is running", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    return 2;
+}
+fn main() {
+    let now = time::Instant::now();
+    let future_one = do_something(1);
+    let outcome = block_on(future_one);
+    println!("time elapsed {:?}", now.elapsed());
+    println!("Here is the outcome: {}", outcome);
+}
