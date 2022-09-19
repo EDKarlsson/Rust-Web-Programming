16fb4a3fddc3725c6988e3e43c0a999062dee67b
Erik Karlsson <erikdkarlsson8@gmail.com>
2022-09-18 18:16:31 -0700

    I think I need to downgrade the versions of actix and serde because the traits aren't being derived correctly for serialization when creating the impl for ToDoItems

--- workspace/ch04/todo_app/src/json_serialization/to_do_items.rs
+++ workspace/ch04/todo_app/src/json_serialization/to_do_items.rs
@@ -1,14 +1,17 @@
-use std::future::pending;
-use serde::Serialize;
-use crate::to_do::ItemTypes;
 use crate::to_do::structs::base::Base;
+use crate::to_do::ItemTypes;
+use actix_web::{web, Error, HttpRequest, HttpResponse, Responder};
+use futures::future::{ready, Ready};
+use serde::Serialize;
+// use std::future::pending;
+use std::vec::Vec;
 
 #[derive(Serialize)]
 pub struct ToDoItems {
     pub pending_items: Vec<Base>,
     pub done_items: Vec<Base>,
     pub pending_item_count: i8,
-    pub done_item_count: i8
+    pub done_item_count: i8,
 }
 
 impl ToDoItems {
@@ -26,9 +29,20 @@ impl ToDoItems {
         let pending_count: i8 = pending_array_buffer.len() as i8;
         return ToDoItems {
             pending_items: pending_array_buffer,
-            done_item_count:done_count,
+            done_item_count: done_count,
             pending_item_count: pending_count,
-            done_items:done_array_buffer
-        }
+            done_items: done_array_buffer,
+        };
+    }
+}
+
+impl Responder for ToDoItems {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
     }
 }
